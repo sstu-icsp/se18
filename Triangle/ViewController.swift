448234c93aee7c528774a94e644dea18bc6c7cd3
nuoroute <nuoroute@gmail.com>
2017-02-27 16:34:40 +0400

    Tweaked ViewController.swift to potentionally allow more unit tests

--- Triangle/ViewController.swift
+++ Triangle/ViewController.swift
@@ -12,6 +12,9 @@
 //               The points are entered by a user as follows: Ax, Ay, Bx, By, Cx, Cy.
 //  Notes:       1. The "user instructions" code works well, but isn't clear.
 //               2. No MVC
+//  WARNING!
+//  THIS VERSION IS TWEAKED SO MORE UNIT TESTS CAN BE PERFORMED. 
+//  THE CODE IS ABSOLUTELY TERRIBLE.
 
 import UIKit
 
@@ -44,31 +47,29 @@ class ViewController: UIViewController {
     ]
     
     @IBAction func sendIt(_ sender: UIButton) {
-        if let value = Double(userInputTextField.text!) {
-            if points.count < 6 {
+        if convertsToDouble(text: userInputTextField.text) {
+            let value = Double(userInputTextField.text!)!
+            
+            if has(less: points, than: 6) {
                 points.append(value)
                 userInputTextField.text = nil
             }
             
-            if points.count == 6 {
-                var alertMessage = UIAlertController()
+            if has(6, points) {
+                var alert = UIAlertController()
                 
-                if calculateAreaOfTriangle(withPoints: points) != 0 {
-                    alertMessage = UIAlertController(
-                        title: String(abs(calculateAreaOfTriangle(withPoints: points))),
-                        message: "is the area of the triangle.",
-                        preferredStyle: UIAlertControllerStyle.alert
+                if calculateAreaOfTriangle(withPoints: points).0! != 0 {
+                    alert = makeAnAlert(
+                        withTitle: String(abs(calculateAreaOfTriangle(withPoints: points).0!)), saying: "is the area of the triangle."
                     )
                 } else {
-                    alertMessage = UIAlertController(
-                        title: "Error",
-                        message: "Presented points don't form a triangle.",
-                        preferredStyle: UIAlertControllerStyle.alert
+                    alert = makeAnAlert(
+                        withTitle: "Error", saying: "Presented points don't form a triangle."
                     )
                 }
                 
-                alertMessage.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
-                self.present(alertMessage, animated: true, completion: nil)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
                 
                 points.removeAll()
                 userInstructionsLabel.text = "Enter the X coordinate of A"
@@ -89,9 +90,41 @@ class ViewController: UIViewController {
         userInstructionsLabel.text = "Enter the " + instructions[points.count]  // Careful here
     }
     
-    func calculateAreaOfTriangle(withPoints points: [Double]) -> Double {
-        let result = 0.5 * ((points[0] - points[4]) * (points[3] - points[1]) - (points[0] - points[2]) * (points[5] - points[1]))
-        
-        return abs(result)
+    func calculateAreaOfTriangle(withPoints points: [Double]) -> (Double?, String?) {
+        if has(6, points) {
+            let result = 0.5 * ((points[0] - points[4]) * (points[3] - points[1]) - (points[0] - points[2]) * (points[5] - points[1]))
+            return (abs(result), nil)
+        } else {
+            return (nil, "The amount of points is incorrect.")
+        }
+    }
+    
+    // Bloat methods for testing
+    func convertsToDouble(text: String?) -> Bool {
+        if let _ = Double(text!) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func has(_ count: Int, _ points: [Double]) -> Bool {
+        if points.count == count {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func has(less points: [Double], than count: Int) -> Bool {
+        if points.count < count {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func makeAnAlert(withTitle title: String, saying message: String) -> UIAlertController {
+        return UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
     }
 }
