9ec04f823258b9a3b28e4c0d09d449f5ce1aa4cc
nuoroute <nuoroute@gmail.com>
2017-02-27 15:17:26 +0400

    Everything seems to work; UI is WIP

--- Triangle/ViewController.swift
+++ Triangle/ViewController.swift
@@ -10,6 +10,7 @@
 //  iOS Applications Testing (Unit Testing)
 //  Description: The program calculates the area of a triangle, given 3 points.
 //               The points are entered by a user as follows: Ax, Ay, Bx, By, Cx, Cy.
+//  Notes:       The "user instructions" code works well, but isn't clear.
 
 import UIKit
 
@@ -18,10 +19,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        userInstructionsLabel.text = "Enter the points:"
+        userInstructionsLabel.text = "Enter the " + instructions[0]
         userInputTextField.keyboardType = UIKeyboardType.numbersAndPunctuation
         userInputTextField.keyboardAppearance = UIKeyboardAppearance.dark
         userInputTextField.autocorrectionType = UITextAutocorrectionType.no
+        userInputTextField.returnKeyType = UIReturnKeyType.next
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,6 +37,11 @@ class ViewController: UIViewController {
     // Store points here
     var points = [Double]()
     
+    // Store instructions here
+    var instructions = [
+        "X coordinate of A", "Y coordinate of A", "X coordinate of B", "Y coordinate of B", "X coordinate of C", "Y coordinate of C"
+    ]
+    
     @IBAction func sendIt(_ sender: UIButton) {
         if let value = Double(userInputTextField.text!) {
             if points.count < 6 {
@@ -43,20 +50,47 @@ class ViewController: UIViewController {
             }
             
             if points.count == 6 {
-                let alertMessage = UIAlertController(
-                    title: String(calculateAreaOfTriangle(withPoints: points)),
-                    message: "is the area of the triangle.",
-                    preferredStyle: UIAlertControllerStyle.alert)
+                var alertMessage = UIAlertController()
+                
+                if calculateAreaOfTriangle(withPoints: points) != 0 {
+                    alertMessage = UIAlertController(
+                        title: String(abs(calculateAreaOfTriangle(withPoints: points))),
+                        message: "is the area of the triangle.",
+                        preferredStyle: UIAlertControllerStyle.alert
+                    )
+                } else {
+                    alertMessage = UIAlertController(
+                        title: "Error",
+                        message: "Presented points don't form a triangle.",
+                        preferredStyle: UIAlertControllerStyle.alert
+                    )
+                }
                 
                 alertMessage.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
                 self.present(alertMessage, animated: true, completion: nil)
+                
                 points.removeAll()
-                userInstructionsLabel.text = "Enter the points:"
+                userInstructionsLabel.text = "Enter the X coordinate of A"
             }
+        } else {
+            userInputTextField.text = nil
+            
+            let alertMessage = UIAlertController(
+                title: "Error",
+                message: "The coordinate you entered is not valid.",
+                preferredStyle: UIAlertControllerStyle.alert
+            )
+            
+            alertMessage.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
         }
+        
+        userInstructionsLabel.text = "Enter the " + instructions[points.count]  // Careful here
     }
     
     func calculateAreaOfTriangle(withPoints points: [Double]) -> Double {
-        return 0.5 * ((points[0] - points[4]) * (points[3] - points[1]) - (points[0] - points[2]) * (points[5] - points[1]))
+        let result = 0.5 * ((points[0] - points[4]) * (points[3] - points[1]) - (points[0] - points[2]) * (points[5] - points[1]))
+        
+        return abs(result)
     }
 }
