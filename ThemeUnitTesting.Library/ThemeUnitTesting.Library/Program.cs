b75a2532d4f6061361b1071a0d523f74b5333887
CristinaNio <niiiooo_o@mail.ru>
2017-03-09 08:37:38 +0400

    Задание 1 выполнено

---
+++ ThemeUnitTesting.Library/ThemeUnitTesting.Library/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*TriangleChecker. Модуль для определения типа треугольника на основе значений длин трёх сторон. Функциональность:
+3.1. Определять Равносторонний, Вырожденный или Неизвестный треугольник.
+3.2. Определять Равнобедренный треугольник.
+3.3. Бросать исключение если длина хотя бы одной из сторон меньше нуля.
+3.4. Определять Прямоугольный треугольник.
+ 
+ * 0-нет такого треугольника
+ 1-НЕизвестный
+ 2-Равнобедренный
+ 3-Равносторонний
+ 4- прямоугольный
+ 5-Прямоугольный и равнобедренный
+ 
+ */
+
+namespace ThemeUnitTesting.Library
+{
+
+    public class Triangle
+    {
+        public static int Pover(double side1, double side2, double side3)
+       {
+           if ((side1 <= 0) || (side2 <= 0) || (side3 <= 0)) { throw new ArgumentException(); }
+
+           if ((side1 + side2 > side3) & (side1 + side3 > side2) & (side2 + side3 > side1))
+           {
+                    if (((side1 > side2) & (side1 > side3) & (side1 * side1 == side2 * side2 + side3 * side3)) ||
+                            ((side2 > side1) & (side2 > side3) & (side2 * side2 == side1 * side1 + side3 * side3)) ||
+                                 ((side3 > side2) & (side3 > side2) & (side3 * side3 == side2 * side2 + side1 * side1)))  return 4;  
+               
+                    if ((side1 == side2)&(side2 == side3)) return 3;
+
+                     if ((side1 == side2) || (side1 == side3) || (side2 == side3)) return 2;
+                return 1; 
+           }
+           else return 0;
+       }
+    }
+
+    public class TriangleTDD
+    { 
+     public static int PvTDD (int s1, int s2, int s3)
+     {
+         if ((s1 <= 0) || (s2 <= 0) || (s3 <= 0)) { throw new ArgumentException(); }
+
+         if ((s1 + s2 > s3) & (s2 + s3 > s1) & (s1 + s3 > s2)) 
+         {
+             if (((s1 > s2) & (s1 > s3) & (s1 * s1 == s2 * s2 + s3 * s3)) ||
+                           ((s2 > s1) & (s2 > s3) & (s2 * s2 == s1 * s1 + s3 * s3)) ||
+                                ((s3 > s2) & (s3 > s2) & (s3 * s3 == s2 * s2 + s1 * s1))) return 4;
+             if ((s1 == s2) & (s1 == s3)) return 3; 
+             if ((s1 == s2) || (s1 == s3) || (s2 == s3)) return 2;
+             
+             
+             return 1; 
+         }
+         else return 0;
+         
+     }
+
+          
+    
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Triangle.Pover(5, 5, Math.Sqrt(50));
+        }
+    }
+}
---
+++ ThemeUnitTesting.Library/ThemeUnitTesting/UnitTest1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ThemeUnitTesting.Library;
+
+namespace ThemeUnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Triangle Instance;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Instance = new Triangle();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Я с нулём.")]
+        public void TestMethod0_0()
+        {
+            int res = Triangle.Pover(0, 3, 4);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Я с -5.")]
+        public void TestMethod0_1()
+        {
+            int res = Triangle.Pover(-5, 3, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Я с -1.")]
+        public void TestMethod0_2()
+        {
+            int res = Triangle.Pover(3, -1, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Я с -4.")]
+        public void TestMethod0_3()
+        {
+            int res = Triangle.Pover(10, 3, -4);
+        }
+        [TestMethod]
+        public void TestMethod_Type2_0()
+        {
+            int res = Triangle.Pover(3, 3, 4);
+
+            Assert.AreEqual(2, res, "Я должен быть равнобедренный");
+        }
+
+        [TestMethod]
+        public void TestMethod_Type2_1()
+        {
+            int res = Triangle.Pover(3, 10, 10);
+
+            Assert.AreEqual(2, res, "Я должен быть равнобедренный");
+        }
+
+        [TestMethod]
+        public void TestMethod_Type2_2()
+        {
+            int res = Triangle.Pover(14, 3, 14);
+
+            Assert.AreEqual(2, res, "Я должен быть равнобедренный");
+        }
+
+        [TestMethod]
+        public void TestMethod_Type3_0()
+        {
+            int res = Triangle.Pover(3, 3, 3);
+
+            Assert.AreEqual(3, res, "Я должен быть равнобедренный");
+        }
+        [TestMethod]
+        public void TestMethod_Type3_1()
+        {
+            int res = Triangle.Pover(13, 13, 13);
+
+            Assert.AreEqual(3, res, "Я должен быть равнобедренный");
+        }
+
+        [TestMethod]
+        public void TestMethod_Type4_0()
+        {
+            int res = Triangle.Pover(5, 4, 3);
+
+            Assert.AreEqual(4, res, "Я должен быть равнобедренный");
+        }
+
+        
+    }
+
+
+
+    [TestClass]
+    public class UnitTDDTest1
+    {
+        private TriangleTDD Instance;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Instance = new TriangleTDD();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Я с ннулями.")]
+        public void TestTDDMethod0_0()
+        {
+            int res = TriangleTDD.PvTDD(0, 0, 0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Я с -6.")]
+        public void TestTDDMethod0_1()
+        {
+            int res = TriangleTDD.PvTDD(1, -6, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Я с -1.")]
+        public void TestTDDMethod0_2()
+        {
+            int res = TriangleTDD.PvTDD(-1, -1, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Я с -10.")]
+        public void TestTDDMethod0_3()
+        {
+            int res = TriangleTDD.PvTDD(10, 3, -10);
+        }
+
+
+        [TestMethod]
+        public void TestTDDMetod_Type1_0()
+        {
+            int res =  TriangleTDD.PvTDD( 6, 7, 8);
+            Assert.AreEqual(1, res, "Я есть");
+        }
+        [TestMethod]
+        public void TestTDDMetod_Type1_1()
+        {
+            int res = TriangleTDD.PvTDD(7,5,6);
+            Assert.AreEqual(1, res, "Я есть");
+        }
+        [TestMethod]
+        public void TestTDDMetod_Type1_2()
+        {
+            int res = TriangleTDD.PvTDD(7, 8, 6);
+            Assert.AreEqual(1, res, "Я есть");
+        }
+
+        [TestMethod]
+        public void TestTDDMetod_Type0_0()
+        {
+            int res = TriangleTDD.PvTDD(17, 8, 6);
+            Assert.AreEqual(0, res, "Я НЕ существую");
+        }
+
+        [TestMethod]
+        public void TestTDDMetod_Type0_1()
+        {
+            int res = TriangleTDD.PvTDD(1, 8, 6);
+            Assert.AreEqual(0, res, "Я НЕ существую");
+        }
+
+        [TestMethod]
+        public void TestTDDMetod_Type0_2()
+        {
+            int res = TriangleTDD.PvTDD(8, 8, 26);
+            Assert.AreEqual(0, res, "Я НЕ существую");
+        }
+
+        [TestMethod]
+        public void TestTDDMethod_Type2_0()
+        {
+            int res = TriangleTDD.PvTDD(3, 3, 4);
+
+            Assert.AreEqual(2, res, "Я должен быть равнобедренный");
+        }
+
+        [TestMethod]
+        public void TestTDDMethod_Type2_1()
+        {
+            int res = TriangleTDD.PvTDD(3, 10, 10);
+
+            Assert.AreEqual(2, res, "Я должен быть равнобедренный");
+        }
+
+        [TestMethod]
+        public void TestTDDMethod_Type2_2()
+        {
+            int res = TriangleTDD.PvTDD(14, 3, 14);
+
+            Assert.AreEqual(2, res, "Я должен быть равнобедренный");
+        }
+
+        [TestMethod]
+        public void TestTDDMethod_Type3_0()
+        {
+            int res = TriangleTDD.PvTDD(3, 3, 3);
+
+            Assert.AreEqual(3, res, "Я должен быть равнобедренный");
+        }
+        [TestMethod]
+        public void TestTDDMethod_Type3_1()
+        {
+            int res = TriangleTDD.PvTDD(13, 13, 13);
+
+            Assert.AreEqual(3, res, "Я должен быть равнобедренный");
+        }
+
+        [TestMethod]
+        public void TestTDDMethod_Type4_0()
+        {
+            int res = TriangleTDD.PvTDD(5, 4, 3);
+
+            Assert.AreEqual(4, res, "Я должен быть равнобедренный");
+        }
+
+
+    }
+}
