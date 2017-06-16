113cf9d4497fd2b8a02677add1178620f9668373
EndLessEmbrace <EndLessEmbrace@yandex.ru>
2017-06-17 00:33:45 +0400

    commit
    commit

---
+++ intRangeTesting/intRange/intRange.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intRanges
+{
+    public class intRange
+    {
+        int x; //левая граница
+        int y; //правая граница
+        int length;
+        bool type; // отрезок/интервал 
+        private bool isEmpty;
+
+        public bool IsEmpty
+        {
+            get { return isEmpty; }
+        }
+
+        public int X
+        {
+            get { return x; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+        }
+
+        public bool Type
+        {
+            get { return type; }
+        }
+
+        public int Lenght
+        {
+            get { return length; }
+        }
+
+        public intRange()
+        {
+            x = 0;
+            y = 0;
+            length = 0;
+        }
+        
+        public intRange(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            length = y - x + 1;
+        }
+
+        public intRange(int x, int y, bool type)
+        {
+            this.x = x;
+            this.y = y;
+            this.type = type;
+            if (type)
+                if (x == y) length = 0;
+                else length = y - x - 1;
+            else length = y - x + 1;
+        }
+
+        public bool Contains(int number)
+        {
+            if (type) return number > this.x && number < this.y;
+            return number >= this.x && number <= this.y;
+        }
+
+        public bool Intersect(intRange range)
+        {
+            if (range.X == x && range.Y == y) return true;
+            if (type)
+            {
+                if (range.X > x && range.X < y) return true;
+                if (range.Y > x && range.Y < y) return true;
+                if (range.X < x && range.Y > y) return true;
+            }
+            else
+            {
+                if (range.X >= x && range.X <= y) return true;
+                if (range.Y >= x && range.Y <= y) return true;
+                if (range.X <= x && range.Y >= y) return true;
+            }
+            return false;
+        }
+
+        public static void Main()
+        {
+
+        }
+
+    }
+}
---
+++ intRangeTesting/intRangeTesting/UnitTest1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using intRanges;
+
+namespace intRangeTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsEmpty()
+        {
+            intRange test = new intRange();
+            Assert.AreEqual(test.IsEmpty, false);
+        }
+
+        [TestMethod]
+        public void NotIsEmpty()
+        {
+            intRange test = new intRange();
+            Assert.AreEqual(!test.IsEmpty, true);
+        }
+
+        [TestMethod]
+        public void LengthCorrect()
+        {
+            intRange test = new intRange(5, 5, true);
+            Assert.AreEqual(test.Lenght, 0);
+        }
+
+        [TestMethod]
+        public void LenghtStretch()
+        {
+            intRange test = new intRange(5, 10, true);
+            Assert.AreEqual(test.Lenght, 4);
+        }
+
+        [TestMethod]
+        public void LenghtInterval()
+        {
+            intRange test = new intRange(5, 10, false);
+            Assert.AreEqual(test.Lenght, 6);
+        }
+
+        [TestMethod]
+        public void ContainsCorrect()
+        {
+            intRange test = new intRange(5, 12);
+            Assert.AreEqual(test.Contains(7),true);
+        }
+
+        [TestMethod]
+        public void ContainsCorrectBorder()
+        {
+            intRange test = new intRange(5, 12);
+            Assert.AreEqual(test.Contains(5), true);
+        }
+
+        [TestMethod]
+        public void ContainsNotCorrect()
+        {
+            intRange test = new intRange(5, 12);
+            Assert.AreEqual(test.Contains(3), false);
+        }
+
+        [TestMethod]
+        public void ContainsIntervalCorrect()
+        {
+            intRange test = new intRange(5, 12,true);
+            Assert.AreEqual(test.Contains(6), true);
+        }
+
+        [TestMethod]
+        public void ContainsIntervalNotCorrect()
+        {
+            intRange test = new intRange(5, 12, true);
+            Assert.AreEqual(test.Contains(3), false);
+        }
+
+        [TestMethod]
+        public void ContainsIntervalBorder()
+        {
+            intRange test = new intRange(5, 12, true);
+            Assert.AreEqual(test.Contains(5), false);
+        }
+
+        [TestMethod]
+        public void IntersectStretch()
+        {
+            intRange testStretch1 = new intRange(0, 10);
+            intRange testStretch2 = new intRange(5, 15);
+            intRange testStretch3 = new intRange(15, 20);
+            Assert.AreEqual(testStretch1.Intersect(testStretch2), true);
+            Assert.AreEqual(testStretch1.Intersect(testStretch1), true);
+            Assert.AreEqual(testStretch2.Intersect(testStretch1), true);
+            Assert.AreEqual(testStretch2.Intersect(testStretch3), true);
+        }
+
+        [TestMethod]
+        public void IntersectInterval()
+        {
+            intRange testInterval1 = new intRange(0, 10, true);
+            intRange testInterval2 = new intRange(5, 15, true);
+            intRange testInterval3 = new intRange(15, 20, true);
+            Assert.AreEqual(testInterval1.Intersect(testInterval2),true);
+            Assert.AreEqual(testInterval1.Intersect(testInterval1), true);
+            Assert.AreEqual(testInterval2.Intersect(testInterval1), true);
+            Assert.AreEqual(testInterval2.Intersect(testInterval3), false);
+        }
+
+        [TestMethod]
+        public void IntersectIntervalStretch()
+        {
+            intRange testInterval1 = new intRange(0, 10, true);
+            intRange testInterval2 = new intRange(15, 20, true);
+            intRange testStretch = new intRange(3, 13, false);
+            Assert.AreEqual(testInterval1.Intersect(testStretch), true);
+            Assert.AreEqual(testStretch.Intersect(testInterval1), true);
+            Assert.AreEqual(testStretch.Intersect(testInterval2), false);
+            Assert.AreEqual(testInterval2.Intersect(testStretch), false);
+        }
+
+    }
+}
